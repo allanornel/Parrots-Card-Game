c68608e882c9d77cb00bf6eeda8c366534c13fcc
allanornel <allanornel@gmail.com>
2022-02-04 19:00:18 -0300

    Project complete with bonus

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ let elemento = document.querySelector(".jogo");
 let jogadaCount = 0;
 let cartasViradas = 0;
 let comparadorCarta = [];
-let fimDoJogo = false;
 let jogando = false;
+let fimDoJogo = false;
 
 verificarRequisitos();
 
@@ -64,6 +64,10 @@ function jogada(elemento) {
         comparadorCarta[cartasViradas] = elemento.innerHTML;
         comparadorCarta[cartasViradas + 2] = elemento;
         cartasViradas++;
+        atualizarJogadas();
+        if (jogadaCount == 1) {
+            startChronometer();
+        }
         if (comparadorCarta[0] == comparadorCarta[1]) {
             comparadorCarta[2].querySelector(".back-face").classList.add("encontrada");
             comparadorCarta[3].querySelector(".back-face").classList.add("encontrada");
@@ -100,12 +104,55 @@ function checarFimDoJogo() {
     jogando = false;
     if (document.querySelectorAll(".encontrada").length == document.querySelectorAll(".card").length) {
         alert(`PARABÉNS, VOCÊ TERMINOU O JOGO EM ${jogadaCount} JOGADAS`);
+        fimDoJogo = true;
         let jogoNovamente = prompt("Deseja jogar novamente? s ou n");
         if (jogoNovamente == "s") {
             document.querySelector(".jogo").innerHTML = "";
             quantidadeCartas = 0;
+            jogadaCount = 0;
             comparadorCarta = [];
+            fimDoJogo = false;
+            T = 0;
+            atualizarJogadas();
             verificarRequisitos();
+
         }
     }
+}
+
+// BONUS
+
+function startChronometer() {
+    T0 = new Date();
+    setTimeout(partialTime, 1000);
+}
+
+function partialTime() {
+    if (!fimDoJogo) {
+        T = Math.trunc((new Date() - T0) / 1000);
+        atualizarRelogio();
+        setTimeout(partialTime, 1000);
+    }
+}
+
+function atualizarRelogio() {
+    var elemento = document.querySelector(".tempo p");
+    elemento.innerHTML = strTempo();
+}
+
+function strTempo() {
+    var seg = T % 60;
+    var min = Math.trunc(T / 60);
+    if (seg < 10) {
+        seg = "0" + seg;
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+    return min + ":" + seg;
+}
+
+function atualizarJogadas() {
+    var elemento = document.querySelector(".jogadas p");
+    elemento.innerHTML = jogadaCount;
 }
